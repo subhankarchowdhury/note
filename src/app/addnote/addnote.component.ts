fb04a2567816fd026cbceb35141a900befb5bca2
subhankar chowdhury <ts-subhank.chowdhury@rakuten.com>
2021-02-09 22:50:39 +0530

    commit

--- src/app/addnote/addnote.component.ts
+++ src/app/addnote/addnote.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-addnote',
   templateUrl: './addnote.component.html',
   styleUrls: ['./addnote.component.scss']
 })
-export class AddnoteComponent implements OnInit {
+export class AddnoteComponent implements OnInit{
 
-  constructor() { }
+  noteForm: FormGroup;
 
-  ngOnInit(): void {
+  constructor(){
+    this.noteForm = new FormGroup({
+      'noteTitle' : new FormControl(null, Validators.required),
+      'noteBody' : new FormControl(null,  Validators.required)
+    })
+  }
+
+  ngOnInit(){
+  }
+
+  onSubmit(){
+    console.log(this.noteForm);
+
+    
+    
   }
 
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from "@angular/core";
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
     imports : [
         MatToolbarModule,
         MatIconModule,
-        MatSidenavModule
+        MatSidenavModule,
+        MatButtonModule
     ],
     exports : [
         MatToolbarModule,
         MatIconModule,
-        MatSidenavModule
+        MatSidenavModule,
+        MatButtonModule
     ]
 })
 export class MaterialModule {}
